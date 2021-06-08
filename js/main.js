0b743b3d984afb1a538cd3a32c45b4ab5f8f872e
Alexander <schidlowski95@tut.bym>
2021-06-08 20:18:32 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -230,3 +230,18 @@
 
 
 }());
+
+// $('a[href^="#"').on('click', function() {
+
+//     let href = $(this).attr('href');
+
+ 
+//     $('html, body').animate({
+//         scrollTop: $(href).offset().top
+//     }, {
+//         duration: 1000,   // по умолчанию «400» 
+//         easing: "linear" // по умолчанию «swing» 
+//     });
+
+//     return false;
+// });
--- js/maps.js
+++ js/maps.js
@@ -3,12 +3,28 @@ function initMap() {
     var element = document.getElementById('map');
     var options = {
         zoom: 12,
-        center: {lat: 55.19115448235065, lng: 30.20450094771684},
+        center: {lat: 55.17096802673227, lng: 30.283152392418877},
     };
 
     var myMap = new google.maps.Map(element, options);
 
     var markers = [
+        {
+            coordinates: {lat: 55.1948330836893, lng: 30.18748333909543},
+            info: '<h3>Остановка: Вокзал</h3><br><img src="https://placehold.it/200x150"><br><p>Описание</p>'
+        },
+        {
+            coordinates: {lat: 55.19117651298901, lng: 30.20587442474107},
+            info: '<h3>Площадь свободы</h3><br><img src="https://placehold.it/200x150"><br><p>Описание</p>'
+        },
+        {
+            coordinates: {lat: 55.177426603844616, lng: 30.227803980924563},
+            info: '<h3>Университет им. П.М. Машерова</h3><br><img src="https://placehold.it/200x150"><br><p>Описание</p>'
+        },
+        {
+            coordinates: {lat: 55.16986977158808, lng: 30.22755598294689},
+            info: '<h3>Ледовый дворец</h3><br><img src="https://placehold.it/200x150"><br><p>Описание</p>'
+        },
         {
             coordinates: {lat: 55.1669302107952, lng: 30.255652732493242},
             info: '<h3>Остановка: Микрорайон "Билево-1"</h3><br><img src="https://placehold.it/200x150"><br><p>Описание</p>'
@@ -21,6 +37,10 @@ function initMap() {
             coordinates: {lat: 55.17270982529784, lng:30.268961093149045},
             info: '<h3>Остановка: ул. Баграмяна</h3><br><img src="https://placehold.it/200x150"><br><p>Описание</p>'
         },
+        {
+            coordinates: {lat: 55.13808803063548, lng:30.41839969854628},
+            info: '<h3>Фазенда Николаево</h3><br><img src="https://placehold.it/200x150"><br><p>Описание</p>'
+        }
 
     ];
 
@@ -48,20 +68,26 @@ function initMap() {
             });
         }
     }
+
+    
+directionsDisplay = new google.maps.DirectionsRenderer();
+
+var request = {
+    origin: new google.maps.LatLng(55.1948330836893,30.18748333909543), //точка старта
+    destination: new google.maps.LatLng(55.13808803063548,30.41839969854628), //точка финиша
+    travelMode: google.maps.DirectionsTravelMode.DRIVING //режим прокладки маршрута
+};
+
+const directionsService = new google.maps.DirectionsService();
+
+directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+    }
+});
+
+directionsDisplay.setMap(myMap);
 }
+https://www.google.com/maps/dir/55.1948330836893,+30.18748333909543/55.19117651298901,+30.20587442474107/55.177426603844616,+30.227803980924563/55.16986977158808,+30.22755598294689/55.1669302107952,+30.255652732493242/55.174049271491064,+30.259055255252076/55.17270982529784,+30.268961093149045/55.13808803063548,+30.41839969854628/@55.1663848,30.2326772,12z/data=!3m1!4b1!4m34!4m33!1m3!2m2!1d30.1874833!2d55.1948331!1m3!2m2!1d30.2058744!2d55.1911765!1m3!2m2!1d30.227804!2d55.1774266!1m3!2m2!1d30.227556!2d55.1698698!1m3!2m2!1d30.2556527!2d55.1669302!1m3!2m2!1d30.2590553!2d55.1740493!1m3!2m2!1d30.2689611!2d55.1727098!1m3!2m2!1d30.4183997!2d55.138088!3e0?hl=ru
 
-// directionsDisplay = new google.maps.DirectionsRenderer();
-
-//     var request = {
-//         origin: new google.maps.LatLng(60.023539414725356,30.283663272857666), //точка старта
-//         destination: new google.maps.LatLng(59.79530896374892,30.410317182540894), //точка финиша
-//         travelMode: google.maps.DirectionsTravelMode.DRIVING //режим прокладки маршрута
-//     };
-
-//     directionsService.route(request, function(response, status) {
-//         if (status == google.maps.DirectionsStatus.OK) {
-//             directionsDisplay.setDirections(response);
-//         }
-//     });
-
-//     directionsDisplay.setMap(map);
+https://goo.gl/maps/xFwJb7wrZQef1Lm18
